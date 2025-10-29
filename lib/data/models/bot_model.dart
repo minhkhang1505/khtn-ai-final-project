5bfeffc6ee1e45b19501234de519e57ade317195
Do Thai Hoc <hocdoth04@gmail.com>
2025-10-29 21:47:04 +0700

    feat:ui chat page

---
+++ lib/data/models/bot_model.dart
@@ -0,0 +1,17 @@
+class BotModel {
+  final String botName;
+  final String botDescription;
+  final String category;
+  final String model;
+  final String state;
+  final String prompt;
+
+  BotModel({
+    required this.botName,
+    required this.botDescription,
+    required this.category,
+    required this.state,
+    required this.model,
+    required this.prompt,
+  });
+}
