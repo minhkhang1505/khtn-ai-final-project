652a1a19f3dd768383d14b31526c5c9aae77c18c
Nguyễn Minh Khang <mkhang040506@gmail.com>
2025-11-07 00:42:50 +0700

    feat: enhance AccountPage layout and add user profile details with upgrade options

--- lib/presentation/views/account/account_page.dart
+++ lib/presentation/views/account/account_page.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:khtn_ai_final_project/core/theme/app_radius.dart';
+import 'package:khtn_ai_final_project/presentation/common/widgets/custom_app_bar.dart';
+
+final avatarSize = 48.0;
 
 /// Account page - User profile and settings
 class AccountPage extends StatelessWidget {
@@ -6,11 +11,324 @@ class AccountPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+    final user = User(
+      id: '1',
+      email: 'user@example.com',
+      username: 'Mockuser',
+      roles: ['admin', 'user'],
+      geo: Geo(lat: '40.7128', long: '-74.0060'),
+    );
     return Scaffold(
-      appBar: AppBar(title: const Text('Account')),
-      body: const Center(
-        child: Text('Implement later', style: TextStyle(fontSize: 18)),
+      appBar: AppBar(
+        centerTitle: false,
+        automaticallyImplyLeading: false,
+        title: Row(
+          children: [
+            SizedBox(
+              width: avatarSize,
+              height: avatarSize,
+              child: CircleAvatar(radius: 40),
+            ),
+            SizedBox(width: 8),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  user.username,
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+                ),
+                Text(user.email, style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ],
+        ),
+      ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(16, 8, 16, 0),
+            child: Column(
+              children: [
+                // Container for upgrade plan
+                Container(
+                  padding: const EdgeInsets.fromLTRB(16, 0, 6, 0),
+                  height: 70,
+                  decoration: BoxDecoration(
+                    color: colorScheme.primary.withAlpha(50),
+                    borderRadius: AppBorderRadius.large,
+                  ),
+                  child: Row(
+                    children: [
+                      SvgPicture.asset(
+                        'assets/icons/ic_upgrade.svg',
+                        colorFilter: ColorFilter.mode(
+                          colorScheme.primary.withAlpha(200),
+                          BlendMode.srcIn,
+                        ),
+                      ),
+                      Text(
+                        "Upgrade to Pro for unlimited access",
+                        style: TextStyle(
+                          //TODO: Add style
+                        ),
+                      ),
+                      Spacer(),
+                      IconButton(
+                        onPressed: () {
+                          //TODO: Add action
+                        },
+                        icon: Icon(Icons.close),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(height: 12),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: colorScheme.outlineVariant.withAlpha(150),
+                      width: 1.5,
+                    ),
+                    color: colorScheme.surfaceContainerLow,
+                    borderRadius: AppBorderRadius.large,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Subscription",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Row(
+                            children: [
+                              Text("Free Plan", style: TextStyle(fontSize: 14)),
+                              SizedBox(width: 6),
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                  horizontal: 6,
+                                ),
+                                decoration: BoxDecoration(
+                                  color: colorScheme.primary,
+                                  borderRadius: AppBorderRadius.small,
+                                ),
+                                child: Text(
+                                  "Current",
+                                  style: TextStyle(
+                                    color: colorScheme.onPrimary,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Text(
+                            "Limited features",
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: colorScheme.onSurface.withAlpha(140),
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(height: 16),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: colorScheme.primary,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: AppBorderRadius.medium,
+                          ),
+                        ),
+                        onPressed: () {
+                          //TODO: Implement theme change
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/ic_upgrade.svg',
+                              colorFilter: ColorFilter.mode(
+                                colorScheme.onPrimary,
+                                BlendMode.srcIn,
+                              ),
+                            ),
+                            Text(
+                              "Upgrade to Pro",
+                              style: TextStyle(color: colorScheme.onPrimary),
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      GestureDetector(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "View Pro Benefits",
+                              style: TextStyle(color: colorScheme.primary),
+                            ),
+                          ],
+                        ),
+                        onTap: () {
+                          //TODO: Implement view pro benefits
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+                // Section Appeariance
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: colorScheme.outlineVariant.withAlpha(150),
+                      width: 1.5,
+                    ),
+                    color: colorScheme.surfaceContainerLow,
+                    borderRadius: AppBorderRadius.large,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Appearance",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 16),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [
+                          Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text("Dark Mode", style: TextStyle(fontSize: 14)),
+                              Text(
+                                "Toggle dark mode theme",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: colorScheme.onSurface.withAlpha(140),
+                                ),
+                              ),
+                            ],
+                          ),
+                          Spacer(),
+                          Switch(
+                            value:
+                                Theme.of(context).brightness == Brightness.dark,
+                            onChanged: (value) {
+                              //TODO: Implement theme change
+                            },
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                // Section Notifications
+                // Section Logout
+                SizedBox(height: 16),
+                Container(
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: colorScheme.outlineVariant.withAlpha(150),
+                      width: 1.5,
+                    ),
+                    color: colorScheme.surfaceContainerLow,
+                    borderRadius: AppBorderRadius.large,
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Account Actions",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: colorScheme.surface,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: AppBorderRadius.medium,
+                            side: BorderSide(
+                              color: colorScheme.errorContainer,
+                              width: 1,
+                            ),
+                          ),
+                        ),
+                        onPressed: () {
+                          //TODO: Implement theme change
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SvgPicture.asset(
+                              'assets/icons/ic_logout.svg',
+                              colorFilter: ColorFilter.mode(
+                                colorScheme.error,
+                                BlendMode.srcIn,
+                              ),
+                            ),
+                            SizedBox(width: 4),
+                            Text(
+                              "Log Out",
+                              style: TextStyle(color: colorScheme.error),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                //version
+                SizedBox(height: 8),
+                Text(
+                  "Version 1.0.0",
+                  style: TextStyle(color: colorScheme.onSurface.withAlpha(140)),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
+}
+
+class User {
+  final String id;
+  final String email;
+  final String username;
+  final List<String> roles;
+  final Geo geo;
+
+  User({
+    required this.id,
+    required this.email,
+    required this.username,
+    required this.roles,
+    required this.geo,
+  });
+}
+
+class Geo {
+  final String lat;
+  final String long;
+
+  Geo({required this.lat, required this.long});
 }
